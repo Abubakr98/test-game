578e4fe6ff08c17c2c6e9a185ed9350b40b77cc7
Abubakr Fazilov <abubakrfazilov98@gmail.com>
2020-09-30 17:47:31 +0300

    teest

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import logo from './logo.svg'
 
 const TestComponent = () => <p>просто render</p>
-const a1dd = 10
+const a1dd = 10;
 const App: React.FC = () => {
   return (
     <div className='App'>
